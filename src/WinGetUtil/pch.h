86f29d83b94c93a08a6811f64cd310e2857fc06a
Hazmee007 <85598201+gifhuppp@users.noreply.github.com>
2021-08-23 02:26:42 +0700

    Initialize release-v0.4.11391-preview

---
+++ src/WinGetUtil/pch.h
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+#pragma once
+
+#define NOMINMAX
+#include <Windows.h>
+
+#pragma warning( push )
+#pragma warning ( disable : 6001 6340 6388 )
+#include <wil/result.h>
+#include <wil/result_macros.h>
+#pragma warning( pop )
+
+#include <algorithm>
+#include <filesystem>
+#include <memory>
+#include <mutex>
+#include <string>
